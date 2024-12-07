5fdaa579bc8efb65056db486665298b847e4421f
boomenghub <12evolte@gmail.com>
2024-12-07 21:31:50 +0700

    clean III

--- project_vgadude_test1.runs/synth_1/top.tcl
+++ project_vgadude_test1.runs/synth_1/top.tcl
@@ -56,11 +56,6 @@ if {$::dispatch::connected} {
 }
 
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param checkpoint.writeSynthRtdsInDcp 1
-set_param synth.incrementalSynthesisCache C:/Users/12evo/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-13000-LAPTOP-EEU2ANOF/incrSyn
-set_msg_config -id {Common 17-41} -limit 10000000
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7a35tcpg236-1
 
